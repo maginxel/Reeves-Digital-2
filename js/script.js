c4c443955f1ab4eb2ecbfacd0b750dd180d618a7
maginxel <naeto.igilige@gmail.com>
2025-06-21 15:15:56 -0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,17 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+const menuToggle = document.querySelector('.menu-toggle');
+const navLinks = document.querySelector('.nav-links');
+
+menuToggle.addEventListener('click', () => {
+    navLinks.classList.toggle('nav-active');
+    menuToggle.classList.toggle('nav-toggle');
+}); 
